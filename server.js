ae75ae6a9328272a88eafdea3f83898165dda999
Anupam Anand <theanupamojha999@gmail.com>
2026-08-10 23:32:49 +0530

    feat : add database

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import mongoose from "mongoose";
+import connectDB from "./src/config/db.js";
 
 import app from "./app.js";
 
-dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection failed:", error);
-  });
