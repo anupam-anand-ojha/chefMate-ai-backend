162923049b2a655f2e48be5ed5f1b8b2a4c3c855
Anupam Anand <theanupamojha999@gmail.com>
2026-08-21 00:15:49 +0530

    fix: ai service

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,24 +1,19 @@
 import { GoogleGenAI } from "@google/genai";
 import recipePrompt from "../prompts/recipe.prompt.js";
 
-
-
 const ai = new GoogleGenAI({
-    apiKey : process.env.GEMINI_API_KEY
-})
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 export const generateRecipe = async (recipeData) => {
+  const interaction = await ai.interactions.create({
+    model: "gemini-3.1-flash-lite",
+    input: recipePrompt(recipeData),
+    response_format: {
+      type: "text",
+      mime_type: "application/json",
+    },
+  });
 
-    const intraction = await ai.interactions.create({
-        model: "gemini-3.1-flash-lite",
-        input: recipePrompt(recipeData),
-        response_format:{
-            type: "text",
-            mime_type: "application/json"
-        }
-    });
-
-    return JSON.parse(intraction.output_text)
-
-    
-}
+  return JSON.parse(interaction.output_text);
+};
